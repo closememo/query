755e2cc4d82a78eb218fc1894d173accd580a748
bitgadak <bitgadak@gmail.com>
2022-01-21 20:07:33 +0900

    gradle, 라이브러리 버전 업

--- src/main/java/com/closememo/query/config/messaging/integration/IntegrationConfig.java
+++ src/main/java/com/closememo/query/config/messaging/integration/IntegrationConfig.java
@@ -1,9 +1,12 @@
 package com.closememo.query.config.messaging.integration;
 
+import static org.reflections.scanners.Scanners.SubTypes;
+
 import com.closememo.query.config.messaging.kafka.KafkaMessageConverter;
 import com.closememo.query.infra.messaging.DomainEvent;
-import java.util.Set;
 import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -22,19 +25,16 @@ import org.springframework.web.context.support.GenericWebApplicationContext;
 @Configuration
 public class IntegrationConfig {
 
-  private final Set<Class<? extends DomainEvent>> domainEvents;
-
-  public IntegrationConfig() {
-    Reflections reflections = new Reflections("com.closememo.query.infra.messaging");
-    domainEvents = reflections.getSubTypesOf(DomainEvent.class);
-  }
-
   @Bean
   public KafkaMessageDrivenChannelAdapter<?, ?> kafkaMessageDrivenAdapter(
       @Qualifier("kafkaListenerContainerFactory") KafkaListenerContainerFactory<?> kafkaListenerContainerFactory,
       @Qualifier("kafkaMessageConverter") KafkaMessageConverter kafkaMessageConverter) {
 
-    String[] topics = domainEvents.stream()
+    Reflections reflections = new Reflections(new ConfigurationBuilder()
+        .setUrls(ClasspathHelper.forClass(DomainEvent.class))
+        .setScanners(SubTypes));
+
+    String[] topics = reflections.getSubTypesOf(DomainEvent.class).stream()
         .map(Class::getSimpleName)
         .toArray(String[]::new);
     ConcurrentMessageListenerContainer<?, ?> container
@@ -62,10 +62,14 @@ public class IntegrationConfig {
   @Autowired
   public void registerDomainEventChannels(GenericWebApplicationContext context,
       @Qualifier("messageTaskExecutor") ThreadPoolTaskExecutor messageTaskExecutor) {
-    domainEvents.forEach(aClass ->
-        context.registerBean(
-            aClass.getSimpleName(),
-            PublishSubscribeChannel.class,
-            () -> new PublishSubscribeChannel(messageTaskExecutor)));
+
+    Reflections reflections = new Reflections(new ConfigurationBuilder()
+        .setUrls(ClasspathHelper.forClass(DomainEvent.class))
+        .setScanners(SubTypes));
+
+    reflections.getSubTypesOf(DomainEvent.class)
+        .forEach(aClass ->
+            context.registerBean(aClass.getSimpleName(), PublishSubscribeChannel.class,
+                () -> new PublishSubscribeChannel(messageTaskExecutor)));
   }
 }
